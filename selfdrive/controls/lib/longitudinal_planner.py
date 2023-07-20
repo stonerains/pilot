68000e41ba8e0a2e8c8444f3a98e092123683733
boxkon <ghkdxo14@gmail.com>
2023-07-20 14:19:34 +0900

    tune: long

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -18,8 +18,8 @@ from system.swaglog import cloudlog
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6, 0.2]
-A_CRUISE_MAX_BP = [0., 20. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
+A_CRUISE_MAX_VALS = [1.6, 1.4, 0.9, 0.7, 0.3]
+A_CRUISE_MAX_BP = [0., 30. * CV.KPH_TO_MS, 50. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 120. * CV.KPH_TO_MS]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
