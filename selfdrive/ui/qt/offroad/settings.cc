f9fa2965737507753cdfee36791e96620de91252
boxkon <ghkdxo14@gmail.com>
2023-11-20 20:07:26 +0900

    fix typo

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -590,7 +590,7 @@ CommunityPanel::CommunityPanel(SettingsWindow *parent) : ListWidget(parent) {
     },
     {
       "SmoothBraking",
-      tr("moother Braking Behind Lead"),
+      tr("Smoother Braking Behind Lead"),
       tr("More natural braking behavior when coming up to a slower vehicle."),
       "../assets/offroad/icon_disengage_on_accelerator.svg",
       },
